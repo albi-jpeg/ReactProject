e5ee704ce0b8de28c1095a2e694d7472b3e5ef8a
Albi <albimelgarlopez@gmail.com>
2025-09-18 19:29:32 +0200

    .

--- README.md
+++
@@ -1,1 +0,0 @@
-Hola! Este es un mini proyecto de prueva con React.js y Vite ( créditos y gracias a midudev por el tutorial:3 ) recuerda clonar el depositorio e instalar dependencias :D
--- projects/hola_mundo_001/src/assets/main.jsx
+++
@@ -1,8 +0,0 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-
-const root = ReactDOM.createRoot(document.getElementById('root'))
-
-root.render(
-    <div>Twitter Card</div>
-)
